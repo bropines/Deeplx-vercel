811dbc6f2a3436c9a6dbf65792fae60065e7c66f
Sergey Pinus <57861007+bropines@users.noreply.github.com>
2024-03-29 02:33:26 +0500

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-    res.json({
-      "code": 200,
-      "message": "DeepL Free API, Developed by sjlleo and missuo. Go to /translate with POST. http://github.com/OwO-Network/DeepLX"
-    });
-  };
-  

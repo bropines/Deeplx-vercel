8d7947b6d926987ce7a4f70ad0d1dc5e4413e8c5
Sergey Pinus <57861007+bropines@users.noreply.github.com>
2024-03-29 02:25:32 +0500

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,7 @@
+module.exports = (req, res) => {
+    res.json({
+      "code": 200,
+      "message": "DeepL Free API, Developed by sjlleo and missuo. Go to /translate with POST. http://github.com/OwO-Network/DeepLX"
+    });
+  };
+  
